191407a963b749b1046d1d4d7e3f40a20237c289
Yastrebov Alexey <kiborg_4@mail.ru>
2020-09-27 22:55:16 +0300

    Added Hibernate and beans

---
+++ java/src/main/java/beans/DetailsEntity.java
@@ -0,0 +1,89 @@
+package beans;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "details", schema = "detailsstock", catalog = "")
+public class DetailsEntity {
+    private int id;
+    private int code;
+    private String description;
+    private String name;
+    private int count;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "code", nullable = false)
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Basic
+    @Column(name = "description", nullable = false, length = 300)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = 50)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "count", nullable = false)
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DetailsEntity that = (DetailsEntity) o;
+
+        if (id != that.id) return false;
+        if (code != that.code) return false;
+        if (count != that.count) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + code;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + count;
+        return result;
+    }
+}
